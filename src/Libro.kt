bf974ec5f407313e289e48b8a80a147e2caa5479
JoseAntonio <jdiabus511@g.educaand.es>
2025-01-30 13:17:16 +0100

    Ejercicio8

---
+++ src/Libro.kt
@@ -0,0 +1,14 @@
+class Libro(val titulo: String, val autor: String, val numPaginas: Int, calificacion: Int) {
+    var calificacion: Int = calificacion
+        set(value) {
+            require(value in 0..10) { "La calificación debe estar entre 0 y 10." }
+            field = value
+        }
+
+    init {
+        require(titulo.isNotBlank()) { "El título no puede estar vacío." }
+        require(autor.isNotBlank()) { "El autor no puede estar vacío." }
+        require(numPaginas > 0) { "El número de páginas debe ser mayor que 0." }
+        require(calificacion in 0..10) { "La calificación debe estar entre 0 y 10." }
+    }
+}
